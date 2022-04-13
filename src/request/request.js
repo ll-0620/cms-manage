ee64f93f47c221c0e3b08cc93de2ff3e8b09e1c3
ylai <2712160374@qq.com>
2022-04-13 21:04:31 +0800

    less解包前

---
+++ src/request/request.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+// 配置項
+const axiosOption = {
+    baseURL: 'http://47.93.114.103:6688/manage',
+    timeout: 5000
+}
+
+// 創建一個單例
+const instance = axios.create(axiosOption);
+
+// 添加請求攔截器
+instance.interceptors.request.use(function (config) {
+  return config;
+}, function (error) {
+  // 對請求錯誤做些什麼
+  return Promise.reject(error);
+});
+
+// 添加響應攔截器
+instance.interceptors.response.use(function (response) {
+  // 對響應數據做點什麼
+  return response.data;
+}, function (error) {
+  // 對響應錯誤做點什麼
+  return Promise.reject(error);
+});
+
+export default instance;
